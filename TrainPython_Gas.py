7048a28d0250ebd0ed5f4cb8461f8b71963ff775
sejaksedari <azaimfuad216@gmail.com>
2022-12-16 14:59:17 +0700

    second commit

--- TrainPython_Gas.py
+++ TrainPython_Gas.py
@@ -1,10 +1,13 @@
 #CATETAN PROGRAMMIZ FUAD
 
-# Terminal Command
+# Terminal Command + Git f(x)
     # CLS -> clear terminal
     # git status -> checking file status within a specified folder
     # git init -> initiate git remote on on a selected folder 
-    #
+    # git add filename.py buat nambahin file tsb ke repo
+    # git commit -m "first commit" - "nama commit"
+    # git remote add origin (linkrepo) buat ngelink ke repo
+    # git push -u origin main buat ngepush 
 
 #Variable
 
@@ -23,10 +26,11 @@ C = int(c)
 
 #Operator
 Multi = A*B*C
+Add = A+B+C
 
 #Printing
 print(A, "times", B, "times", C, "equals: ", Multi)
-
+print(A, "add", B, "add", C, "equals: ", Add)
 
 
 #_____________________________________________________________________________
