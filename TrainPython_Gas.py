93c61a2cf9028b65dcd72fb95dde1a09de0f0fed
sejaksedari <azaimfuad216@gmail.com>
2022-12-16 14:44:42 +0700

    Initial version

---
+++ TrainPython_Gas.py
@@ -0,0 +1,33 @@
+#CATETAN PROGRAMMIZ FUAD
+
+# Terminal Command
+    # CLS -> clear terminal
+    # git status -> checking file status within a specified folder
+    # git init -> initiate git remote on on a selected folder 
+    #
+
+#Variable
+
+#_____________________________________________________________________________
+#Operator&Operrands
+
+#Declare
+a = input("Insert Integer a: ")
+A = int(a)
+
+b = input("Insert Integer b: ")
+B = int(b)
+
+c = input("Insert Integer c: ")
+C = int(c)
+
+#Operator
+Multi = A*B*C
+
+#Printing
+print(A, "times", B, "times", C, "equals: ", Multi)
+
+
+
+#_____________________________________________________________________________
+#Operator&Operrands
