8b7acb2aa8950152bb69b2b155a21f3b036588f3
sejaksedari <azaimfuad216@gmail.com>
2022-12-16 15:44:14 +0700

    third commit

--- TrainPython_Gas.py
+++ TrainPython_Gas.py
@@ -8,13 +8,50 @@
     # git commit -m "first commit" - "nama commit"
     # git remote add origin (linkrepo) buat ngelink ke repo
     # git push -u origin main buat ngepush 
+    # kalo abis make change, cara push commit
+        # git add . 
+        # git status
+        # git commit -m "second commit" - "nama commit"
+        # git push
+    # Link cara commit, push, dll -> https://www.youtube.com/watch?v=eGaImwD8fPQ&ab_channel=VedTheMaster
+    # Link tambahan -> https://stackoverflow.com/questions/827351/push-origin-master-error-on-new-repository/6518774#6518774
 
-#Variable
 
-#_____________________________________________________________________________
-#Operator&Operrands
+# PythonVariables_____________________________________________________________________
+    # Used to store data (values)
+        # Eg. a = 10, b = 1.1, a & b itu variables. Bacanya, variable a storing value 10
+    # Ibaratnya variable itu bag to store books in it, booksnya bisa diganti at any time
+        # Eg. a = 10, a =1, awalnya value a 10 trs ganti jadi 1
+    # Aturan penamaan
+        # a to z, A to Z, 0 to 9 atau _ 
+        # ga bisa start a variable name with a digit
+        # pake underscore buat gantiin spasi
+        # ada beberapa word yg ga bisa dipake krn dia functions misal: if, for, except, try, or, etc. (kalo maksa masukin ini bakal ada feedback syntax error)
+        # Eg. number, salary, first_name, number1
+    #Python_Literals
+        # number = 5.5 | number itu variabelnya, 5.5 itu literal
+        # jenis literalnya ada numeric, string, boolean
+        # Numeric_Literals
+            # Integers = 5, 0, -45
+            # Floats = 3.4, 1.3, 1.52e2 = 1.525*10^2, e = 10^
+            # Numeric_literals can also be represented in binary, hexadecial, or octal systems
+            # Eg. a = 0b1011 -> binary, b = 0o310 -> Octal, c = 0x12c -> hexadecimal
+        # String_Literals
+            # a sequence of characters surrounded by quotes, bisa pake single, double, or triple quotes for a string
+            # triple quotes """___""" buat multiline string, jadi output printnya bisa >1 baris
+        # Boolean_Literals
+            # True, False
+            # a = true -> error
+                # dia case sensitive, kalo kita tulis a = true -> error
+            # a = True -> assigned True to the variable 'a'
+            # a = 'True' -> ga error sih, tapi 'True' is stored as string 
+            
 
-#Declare
+
+
+# Operator&Operrands___________________________________________________________
+
+# Declare
 a = input("Insert Integer a: ")
 A = int(a)
 
@@ -24,14 +61,13 @@ B = int(b)
 c = input("Insert Integer c: ")
 C = int(c)
 
-#Operator
+# Operator
 Multi = A*B*C
 Add = A+B+C
 
-#Printing
+# Printing
 print(A, "times", B, "times", C, "equals: ", Multi)
 print(A, "add", B, "add", C, "equals: ", Add)
 
 
-#_____________________________________________________________________________
-#Operator&Operrands
+#Operator&Operrands_____________________________________________________________________________
